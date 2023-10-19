2edcfaf8758dfa80268799b33f294f76d2f18fa4
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-10-19 22:55:23 +0200

    Revert "Revert "comments containing part of the plan""
    This reverts commit 37a35258bc32bdea95b48081881260022c6303b0.

---
+++ Day27MileKilometerConverter/main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+# use radiobutton to choose conversion direction
+
+# def radio_used():
+#     print(radio_state.get())
+#
+#
+# # Variable to hold on to which radio button value is checked.
+# radio_state = IntVar()
+# radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+# radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+# radiobutton1.pack()
+# radiobutton2.pack()
