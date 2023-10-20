40c7e37cc1f6afcda1c03d8d317e9e69ddc89338
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-10-20 17:08:49 +0200

    basic labels

--- Day27MileKilometerConverter/main.py
+++ Day27MileKilometerConverter/main.py
@@ -20,6 +20,17 @@ window.minsize(width=500, height=300)
 window.maxsize(width=500, height=300)
 
 # Label
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+equal_label = Label(text="is equal to")
+equal_label.grid(column=0, row=1)
+
+result_label = Label(text="0")
+result_label.grid(column=1, row=1)
+
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
 
 # Entry
 
