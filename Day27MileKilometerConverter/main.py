0437bc0fe3aa89c5169a94a4127b80dedc022ae2
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-10-20 17:11:14 +0200

    window padding and fonts for better readability

--- Day27MileKilometerConverter/main.py
+++ Day27MileKilometerConverter/main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+FONT = ("arial", 20)
+
 # use radiobutton to choose conversion direction
 
 # def radio_used():
@@ -18,18 +20,19 @@ window = Tk()
 window.title("Mile <> Kilometer Converter")
 window.minsize(width=500, height=300)
 window.maxsize(width=500, height=300)
+window.config(padx=50, pady=50)
 
 # Label
-miles_label = Label(text="Miles")
+miles_label = Label(text="Miles", font=FONT)
 miles_label.grid(column=2, row=0)
 
-equal_label = Label(text="is equal to")
+equal_label = Label(text="is equal to", font=FONT)
 equal_label.grid(column=0, row=1)
 
-result_label = Label(text="0")
+result_label = Label(text="0", font=FONT)
 result_label.grid(column=1, row=1)
 
-km_label = Label(text="Km")
+km_label = Label(text="Km", font=FONT)
 km_label.grid(column=2, row=1)
 
 # Entry
