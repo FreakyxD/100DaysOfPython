ce4dd7a91c26f97b4e938b454cca4d7564279e3c
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-10-20 17:14:01 +0200

    adjust window padding padding for each label

--- Day27MileKilometerConverter/main.py
+++ Day27MileKilometerConverter/main.py
@@ -20,20 +20,24 @@ window = Tk()
 window.title("Mile <> Kilometer Converter")
 window.minsize(width=500, height=300)
 window.maxsize(width=500, height=300)
-window.config(padx=50, pady=50)
+window.config(padx=25, pady=25)
 
 # Label
 miles_label = Label(text="Miles", font=FONT)
 miles_label.grid(column=2, row=0)
+miles_label.config(padx=5, pady=5)
 
 equal_label = Label(text="is equal to", font=FONT)
 equal_label.grid(column=0, row=1)
+equal_label.config(padx=5, pady=5)
 
 result_label = Label(text="0", font=FONT)
 result_label.grid(column=1, row=1)
+result_label.config(padx=5, pady=5)
 
 km_label = Label(text="Km", font=FONT)
 km_label.grid(column=2, row=1)
+km_label.config(padx=5, pady=5)
 
 # Entry
 
