8c438cf049e5dace8af1c5e794bcf9a50d5cdb87
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-10-20 18:54:49 +0200

    empty line for readability adjust rows accordingly define radiobutton labels remove unnecessary window size > rely on padding

--- Day27MileKilometerConverter/main.py
+++ Day27MileKilometerConverter/main.py
@@ -16,8 +16,6 @@ def radio_used():
 # Window
 window = Tk()
 window.title("Mile > Kilometer Converter")
-window.minsize(width=350, height=175)
-window.maxsize(width=350, height=175)
 window.config(padx=25, pady=25)
 
 # Label
@@ -25,36 +23,40 @@ convert_choice_label = Label(text="Convert to:", font=FONT)
 convert_choice_label.grid(column=0, row=0)
 
 miles_label = Label(text="Miles", font=FONT)
-miles_label.grid(column=2, row=1)
+miles_label.grid(column=2, row=2)
 miles_label.config(padx=5, pady=5)
 
 equal_label = Label(text="is equal to", font=FONT)
-equal_label.grid(column=0, row=2)
+equal_label.grid(column=0, row=3)
 equal_label.config(padx=5, pady=5)
 
 result_label = Label(text="0", font=FONT)
-result_label.grid(column=1, row=2)
+result_label.grid(column=1, row=3)
 result_label.config(padx=5, pady=5)
 
 km_label = Label(text="Km", font=FONT)
-km_label.grid(column=2, row=2)
+km_label.grid(column=2, row=3)
 km_label.config(padx=5, pady=5)
 
 # Radio
 radio_state = IntVar()  # Variable to hold on to which radio button value is checked
 
-radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton1 = Radiobutton(text="Miles", value=1, variable=radio_state, command=radio_used)
 radiobutton1.grid(column=1, row=0)
 
-radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Km", value=2, variable=radio_state, command=radio_used)
 radiobutton2.grid(column=2, row=0)
 
 # Entry
 user_input = Entry(width=10)
-user_input.grid(column=1, row=1)
+user_input.grid(column=1, row=2)
+
+# Empty grid row
+empty_label = Label(text="")
+empty_label.grid(column=0, row=1)
 
 # Button
 calc_button = Button(text="Calculate", command=convert)
-calc_button.grid(column=1, row=3)
+calc_button.grid(column=1, row=4)
 
 window.mainloop()
