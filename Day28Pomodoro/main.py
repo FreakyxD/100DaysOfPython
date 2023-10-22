41316d347aa4a8c9c96c8234b0893249cf9ee36b
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-10-22 15:24:47 +0200

    grid instead of pack

--- Day28Pomodoro/main.py
+++ Day28Pomodoro/main.py
@@ -24,16 +24,18 @@ canvas = Canvas(width=200, height=224, background=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
 # label
 timer_label = Label(text="Timer")
 timer_label.grid(column=1, row=0)
 checkmark_label = Label(text="✔")
+checkmark_label.grid(column=1, row=3)
 
 # buttons
 start_btn = Button(text="Start")
+start_btn.grid(column=0, row=2)
 reset_btn = Button(text="Reset")
-
+reset_btn.grid(column=2, row=2)
 
 window.mainloop()
