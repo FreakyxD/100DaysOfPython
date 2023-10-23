022101a01a220bfe443eaf5662dc3273709a9bff
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-10-23 11:49:16 +0200

    general ui setup

--- Day29PasswordManager/main.py
+++ Day29PasswordManager/main.py
@@ -1,5 +1,17 @@
+from tkinter import *
+
 # Password generator
 
 # Save Password
 
 # UI Setup
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+logo = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200)
+canvas.grid(column=1, row=1)
+canvas.create_image(100, 100, image=logo)
+
+window.mainloop()
