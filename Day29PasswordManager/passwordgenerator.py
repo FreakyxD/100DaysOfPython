a4344eb416f5783cbffbad1cc0cf3483325cc425
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-10-25 17:48:18 +0200

    optimizing for loops with list comprehension

--- Day29PasswordManager/passwordgenerator.py
+++ Day29PasswordManager/passwordgenerator.py
@@ -12,17 +12,11 @@ def generate_password():
     nr_symbols = random.randint(2, 4)
     nr_numbers = random.randint(2, 4)
 
-    password_list = []
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
 
-    for char in range(nr_letters):
-        password_list.append(random.choice(letters))
-
-    for char in range(nr_symbols):
-        password_list += random.choice(symbols)
-
-    for char in range(nr_numbers):
-        password_list += random.choice(numbers)
-
+    password_list = password_letters + password_symbols + password_numbers
     random.shuffle(password_list)
 
     password = ""
