3c948b4828183720970def0d65cacbeb7c4655a3
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-10-29 00:59:30 +0200

    Move main logic to a function and implement repeat on error

--- Day26NatoAlphabet/nato.py
+++ Day26NatoAlphabet/nato.py
@@ -10,12 +10,18 @@ df = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 print(nato_dict)
 
-# Creating a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word: ").upper()
 
-try:
-    output = [nato_dict[letter] for letter in user_input]
-except KeyError:
-    print("Sorry, only letters in the alphabet please.")
-else:
-    print(output)
+# Creating a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+
+    try:
+        output = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output)
+
+
+generate_phonetic()
