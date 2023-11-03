2d5f3da8415bfcbefc8149362cb1fb41c18647ef
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-11-03 13:34:15 +0100

    define foreign language depending on CSV data

--- Day31FlashCards/main.py
+++ Day31FlashCards/main.py
@@ -7,6 +7,7 @@ BACKGROUND_COLOR = "#B1DDC6"
 df = pd.read_csv("data/100_croatian_words.csv")
 df.drop("Word Frequency", axis=1, inplace=True)
 word_list = df.to_dict("records")
+foreign_langauge = list(word_list[0].keys())[0]  # define foreign language depending on CSV data
 
 # UI Setup
 window = Tk()
