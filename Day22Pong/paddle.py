8408dd8c58594b7bf704386554e5b5bb6a5afa70
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-11-05 14:38:56 +0100

    Commit of already completed project

---
+++ Day22Pong/paddle.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+
+        self.shape("square")
+        self.color("white")
+        # default size is 20
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+        self.width(20)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(x=self.xcor(), y=new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(x=self.xcor(), y=new_y)
