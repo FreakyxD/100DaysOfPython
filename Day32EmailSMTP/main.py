9a1e8b07b521445008f7143b9526a7552d8ef32c
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-11-05 13:56:09 +0100

    avoiding connection.close()

--- Day32EmailSMTP/main.py
+++ Day32EmailSMTP/main.py
@@ -1,12 +1,12 @@
 import smtplib
 from credentials import MY_EMAIL, MY_PASSWORD, SMTP_SERVER, SMTP_SERVER_PORT, TO_ADDRESS
 
-connection = smtplib.SMTP(SMTP_SERVER, SMTP_SERVER_PORT)
-connection.starttls()
-connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-connection.sendmail(
-    from_addr=MY_EMAIL,
-    to_addrs=TO_ADDRESS,
-    msg="Subject: My first subject\n\nJust an email body"
-)
-connection.close()
+# avoid connection.close()
+with smtplib.SMTP(SMTP_SERVER, SMTP_SERVER_PORT) as connection:
+    connection.starttls()
+    connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+    connection.sendmail(
+        from_addr=MY_EMAIL,
+        to_addrs=TO_ADDRESS,
+        msg="Subject: My first subject\n\nJust an email body"
+    )
