80383c2563b252c6e8a974bc94c8fd2493132418
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-11-05 13:28:44 +0100

    smtp server port

--- Day32EmailSMTP/main.py
+++ Day32EmailSMTP/main.py
@@ -1,7 +1,7 @@
 import smtplib
-from credentials import MY_EMAIL, MY_PASSWORD, SMTP_SERVER, TO_ADDRESS
+from credentials import MY_EMAIL, MY_PASSWORD, SMTP_SERVER, SMTP_SERVER_PORT, TO_ADDRESS
 
-connection = smtplib.SMTP(SMTP_SERVER)
+connection = smtplib.SMTP(SMTP_SERVER, SMTP_SERVER_PORT)
 connection.starttls()
 connection.login(user=MY_EMAIL, password=MY_PASSWORD)
 connection.sendmail(from_addr=MY_EMAIL, to_addrs=TO_ADDRESS)
