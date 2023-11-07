5fc092f39727f4a2294faa4b6dab90e2f5cc3706
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-11-07 22:57:12 +0100

    Committing project BirthdayWisher sends a randomized birthday greeting via email depending on an input csv

---
+++ Day32EmailSMTP/BirthdayWisher/main.py
@@ -0,0 +1,49 @@
+from Day32EmailSMTP.credentials import MY_EMAIL, MY_PASSWORD, SMTP_SERVER, SMTP_SERVER_PORT
+import smtplib
+import pandas as pd
+import datetime as dt
+import random
+
+# csv input format:
+# name,email,year,month,day
+# Test,test@email.com,1961,12,21
+df = pd.read_csv("birthdays.csv")
+birthday_dict = df.to_dict("records")
+
+
+def generate_letter(recipient):
+    recipient_name = recipient["name"]
+
+    # pick random letter number
+    l_number = random.randint(1, 3)
+
+    with open(f"letter_templates/letter_{l_number}.txt", mode="r") as letter:
+        prepared_letter = letter.read().replace("[NAME]", recipient_name)
+        return prepared_letter
+
+
+def send_letter(letter_text, recipient):
+    with smtplib.SMTP(SMTP_SERVER, SMTP_SERVER_PORT) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=recipient,
+            msg=f"Subject: Happy Birthday!\n\n{letter_text}"
+        )
+
+
+# Check if today matches a birthday in the birthdays.csv
+now = dt.datetime.now()
+current_year = now.year
+current_month = now.month
+current_day = now.day
+
+for person_dict in birthday_dict:
+    birthday_month = person_dict["month"]
+    birthday_day = person_dict["day"]
+    recipient_email = person_dict["email"]
+
+    if birthday_day == current_day and birthday_month == current_month:
+        final_letter = generate_letter(person_dict)
+        send_letter(final_letter, recipient_email)
