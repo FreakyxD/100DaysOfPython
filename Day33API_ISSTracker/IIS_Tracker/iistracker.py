35f64b4e44ed5662ab751bc1de790c5f6f847efb
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-11-16 11:12:07 +0100

    functions for iis position and to check whether it's dark

--- Day33API_ISSTracker/IIS_Tracker/iistracker.py
+++ Day33API_ISSTracker/IIS_Tracker/iistracker.py
@@ -1,26 +1,39 @@
 from datetime import datetime
-
 import requests
 from Day33API_ISSTracker.locationdata import MY_LAT, MY_LONG
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
 
-iis_latitude = response.json()["iss_position"]["latitude"]
-iis_longitude = response.json()["iss_position"]["longitude"]
+def get_iss_position():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+    iis_latitude = data["iss_position"]["latitude"]
+    iis_longitude = data["iss_position"]["longitude"]
+    return iis_latitude, iis_longitude
 
-response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
 
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+def is_dark():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
 
-time_now = datetime.now()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+    hour_now = time_now.hour
+
+    if sunrise <= hour_now <= sunset:
+        return False
+    else:
+        return True
+
+
+get_iss_position()
+is_dark()
