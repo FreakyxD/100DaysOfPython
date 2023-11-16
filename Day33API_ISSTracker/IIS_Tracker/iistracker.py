2db6074e6a6189f28b0739a26ae34f95716f5e55
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-11-16 20:49:18 +0100

    function for checking if iis is close and one to send an email

--- Day33API_ISSTracker/IIS_Tracker/iistracker.py
+++ Day33API_ISSTracker/IIS_Tracker/iistracker.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import requests
 from Day33API_ISSTracker.locationdata import MY_LAT, MY_LONG
+from smtplib import SMTP
+from Day32EmailSMTP.credentials import SMTP_SERVER, SMTP_SERVER_PORT, MY_EMAIL, MY_PASSWORD, TO_ADDRESS
 
 
 def get_iss_position():
@@ -35,5 +37,40 @@ def is_dark():
         return True
 
 
-get_iss_position()
+def is_iis_close():
+    """Returns True if the ISS is within 5 degrees +/- of my current location"""
+    # TODO each lat and lng need to be +-5 of current lat and long
+    # then, return true
+    coordinates = get_iss_position()
+    iis_lat = coordinates[0]
+    iis_lng = coordinates[1]
+
+    lat_difference = iis_lat - MY_LAT
+    lng_difference = iis_lng - MY_LONG
+
+    if -5 <= lat_difference <= 5 and -5 <= lng_difference <= 5:
+        print("within +-5")
+        return True
+    else:
+        print("not close")
+        return False
+
+
+def send_mail():
+    # debug
+    if True:
+        print("Subject: IIS is right above you\n\nLook up!")
+        return
+    with SMTP(SMTP_SERVER, SMTP_SERVER_PORT) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=TO_ADDRESS,
+            msg="Subject: IIS is right above you\n\nLook up!"
+        )
+
+
+# if is_iis_close() and is_dark() --> send mail to look up
+is_iis_close()
 is_dark()
