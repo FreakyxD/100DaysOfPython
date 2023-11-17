27b78bcc3f7b2fdf90c29932ddfe7ed0113caa81
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-11-17 20:37:52 +0100

    making this more apparent

--- Day34QuizzApp/data.py
+++ Day34QuizzApp/data.py
@@ -9,7 +9,9 @@ parameters = {
 
 response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
-question_data = response.json()["results"]
+data = response.json()
+
+question_data = data["results"]
 
 # question_data = [
 #     {
