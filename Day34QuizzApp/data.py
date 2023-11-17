e4202c28e05d32586e0a24ca3cd5fbf6eae3d076
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-11-17 20:29:38 +0100

    Using api to get questions each time instead of static dict

--- Day34QuizzApp/data.py
+++ Day34QuizzApp/data.py
@@ -1,105 +1,111 @@
+import requests
+
 # get new data https://opentdb.com/api_config.php
 
-question_data = [
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "hard",
-        "question": "In The Sims series, the most members in a household you can have is 8.",
-        "correct_answer": "True",
-        "incorrect_answers": ["False"]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In Mario Kart 64, Waluigi is a playable character.",
-        "correct_answer": "False",
-        "incorrect_answers": ["True"]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "The Ace Attorney trilogy was suppose to end with Phoenix Wright: Ace Attorney - Trials "
-                    "and Tribulations as its final game.",
-        "correct_answer": "True",
-        "incorrect_answers": ["False"]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "Gordon Freeman, the protagonist of Half-Life, is said to have once had a ponytail.",
-        "correct_answer": "True",
-        "incorrect_answers": ["False"]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "Minecraft can be played with a virtual reality headset.",
-        "correct_answer": "True",
-        "incorrect_answers": ["False"]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "hard",
-        "question": "Danganronpa 2: Goodbye Despair featured all of the surviving students from the first game.",
-        "correct_answer": "False",
-        "incorrect_answers": ["True"]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In Splatoon, the Squid Sisters are named Tako and Yaki.",
-        "correct_answer": "False",
-        "incorrect_answers": ["True"]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "The names of Tom Nook's cousins in the Animal Crossing franchise are named Timmy "
-                    "and Jimmy.",
-        "correct_answer": "False",
-        "incorrect_answers": ["True"]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In Heroes of the Storm, the Cursed Hollow map gimmick requires players to kill the undead to curse "
-                    "the enemy team.",
-        "correct_answer": "False",
-        "incorrect_answers": ["True"]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "In Call Of Duty: World At War, the first appearance of the PPSH-41 in "
-                    "Zombies was in the map Der Riese.",
-        "correct_answer": "False",
-        "incorrect_answers": ["True"]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "Doki Doki Literature Club was developed in Japan.",
-        "correct_answer": "False",
-        "incorrect_answers": ["True"]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In the video game Splatoon, the playable characters were originally going to be rabbits "
-                    "instead of squids.",
-        "correct_answer": "True",
-        "incorrect_answers": ["False"]
-    }
-]
+response = requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean")
+response.raise_for_status()
+question_data = response.json()["results"]
+
+# question_data = [
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "hard",
+#         "question": "In The Sims series, the most members in a household you can have is 8.",
+#         "correct_answer": "True",
+#         "incorrect_answers": ["False"]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "In Mario Kart 64, Waluigi is a playable character.",
+#         "correct_answer": "False",
+#         "incorrect_answers": ["True"]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "The Ace Attorney trilogy was suppose to end with Phoenix Wright: Ace Attorney - Trials "
+#                     "and Tribulations as its final game.",
+#         "correct_answer": "True",
+#         "incorrect_answers": ["False"]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "Gordon Freeman, the protagonist of Half-Life, is said to have once had a ponytail.",
+#         "correct_answer": "True",
+#         "incorrect_answers": ["False"]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "Minecraft can be played with a virtual reality headset.",
+#         "correct_answer": "True",
+#         "incorrect_answers": ["False"]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "hard",
+#         "question": "Danganronpa 2: Goodbye Despair featured all of the surviving students from the first game.",
+#         "correct_answer": "False",
+#         "incorrect_answers": ["True"]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "In Splatoon, the Squid Sisters are named Tako and Yaki.",
+#         "correct_answer": "False",
+#         "incorrect_answers": ["True"]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "The names of Tom Nook's cousins in the Animal Crossing franchise are named Timmy "
+#                     "and Jimmy.",
+#         "correct_answer": "False",
+#         "incorrect_answers": ["True"]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "In Heroes of the Storm, the Cursed Hollow map gimmick requires players to kill the undead to curse "
+#                     "the enemy team.",
+#         "correct_answer": "False",
+#         "incorrect_answers": ["True"]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "In Call Of Duty: World At War, the first appearance of the PPSH-41 in "
+#                     "Zombies was in the map Der Riese.",
+#         "correct_answer": "False",
+#         "incorrect_answers": ["True"]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "Doki Doki Literature Club was developed in Japan.",
+#         "correct_answer": "False",
+#         "incorrect_answers": ["True"]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "In the video game Splatoon, the playable characters were originally going to be rabbits "
+#                     "instead of squids.",
+#         "correct_answer": "True",
+#         "incorrect_answers": ["False"]
+#     }
+# ]
