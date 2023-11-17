874a840fb4ca2a893763df00f808e8caab488ea3
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-11-17 20:52:25 +0100

    groundwork for UI create class import class in main.py comment out an existing & conflicting mainloop

--- Day34QuizzApp/main.py
+++ Day34QuizzApp/main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 
@@ -12,9 +13,10 @@ for q_dict in question_data:
     question_bank.append(new_question)
 
 go_quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface()
 
-while go_quiz.still_has_questions():
-    go_quiz.next_question()
+# while go_quiz.still_has_questions():
+#     go_quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {go_quiz.score}/{go_quiz.question_number}")
