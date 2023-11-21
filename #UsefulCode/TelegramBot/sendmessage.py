727233c566fe70531a7577cea083cd8474c2e84b
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-11-21 21:34:48 +0100

    init

---
+++ #UsefulCode/TelegramBot/sendmessage.py
@@ -0,0 +1,17 @@
+import requests
+from secrets import telegram_bot_token, telegram_bot_chat_ID
+
+
+def send_message_to_telegram_bot(message):
+    telegram_bot_url = "https://api.telegram.org/bot" + telegram_bot_token + "/sendMessage"
+    telegram_parameters = {
+        "chat_id": telegram_bot_chat_ID,
+        "parse_mode": "Markdown",
+        "text": message
+    }
+    telegram_response = requests.get(url=telegram_bot_url, params=telegram_parameters)
+    telegram_response.raise_for_status()
+    print(f"Telegram response: {telegram_response}")
+
+
+send_message_to_telegram_bot("Sample Message")
---
+++ #UsefulCode/WeatherReport/rainforecast.py
@@ -0,0 +1,37 @@
+import requests
+from secrets import api_key
+from locationdata import MY_LOCATION
+
+
+# current https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+# forecast https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+
+URL = "https://api.openweathermap.org/data/2.8/onecall"
+parameters = {
+    "lat": MY_LOCATION["latitude"],
+    "lon": MY_LOCATION["longitude"],
+    "units": "metric",
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
+}
+
+response = requests.get(url=URL, params=parameters)
+response.raise_for_status()
+data = response.json()
+print(f"Response code: {response.status_code}")
+
+# If weather code ID < 700 -> bring umbrella
+hourly_forecast = data["hourly"]
+
+need_umbrella = False
+for hour in hourly_forecast[:12]:  # iterate through the next 11 hours
+    for weather in hour["weather"]:  # iterate through all weather conditions
+        if weather["id"] < 700:
+            need_umbrella = True
+            break
+    if need_umbrella:
+        break
+
+if need_umbrella:
+    print("It's going to rain today. Remember to bring an ☔️")
