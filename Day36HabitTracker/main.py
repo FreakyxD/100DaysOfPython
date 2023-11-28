c30b6416a5b3d7368d5b21deeb45fb32b732ee4b
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-11-28 18:43:27 +0100

    init creating a pixela user

--- Day36HabitTracker/main.py
+++ Day36HabitTracker/main.py
@@ -1,8 +1,10 @@
 import requests
 from auth import PIXELA_TOKEN
-from sensitive import PIXELA_USERNAME
+from sensitive import PIXELA_USERNAME, PIXELA_GRAPH_ID, PIXELA_TIMEZONE
 
 pixela_endpoint = "https://pixe.la/v1/users"
+graph_endpoint = f"{pixela_endpoint}/{PIXELA_USERNAME}/graphs"
+graph_endpoint_id = graph_endpoint + f"/{PIXELA_GRAPH_ID}"
 
 parameters = {
     "token": PIXELA_TOKEN,
@@ -12,5 +14,30 @@ parameters = {
 }
 
 # Creating user - only once!
-response = requests.post(url=pixela_endpoint, json=parameters)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=parameters)
+# print(response.text)
+
+
+graph_config = {
+    "id": PIXELA_GRAPH_ID,
+    "name": "100 Days of Python",
+    "unit": "hours",
+    "type": "float",
+    "color": "sora",
+}
+
+headers = {
+    "X-USER-TOKEN": PIXELA_TOKEN,
+}
+
+# Creating graph - only once!
+# response = requests.post(url=graph_endpoint, headers=headers, json=graph_config)
+# print(response.text)
+
+
+# Changing graph
+# graph_config_changes = {
+#     "timezone": PIXELA_TIMEZONE
+# }
+# response = requests.post(url=graph_endpoint_id, headers=headers, json=graph_config_changes)
+# print(response)
