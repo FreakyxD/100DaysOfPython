0444fddf7a33b3b6ad11a6e9f7d4f071fd67da71
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-12-01 23:14:04 +0100

    implementing main logic get and update sheet content

--- Day39FlightFinder/data_manager.py
+++ Day39FlightFinder/data_manager.py
@@ -1,3 +1,44 @@
+from auth import SHEETY_BEARER_TOKEN
+from sensitive import SHEETY_PRIVATE_ENDPOINT_PRICES
+import requests
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+
+    def __init__(self):
+        self.BEARER_TOKEN = SHEETY_BEARER_TOKEN
+        self.ENDPOINT = SHEETY_PRIVATE_ENDPOINT_PRICES
+
+    def get_sheet_content(self):
+        response = requests.get(url=self.ENDPOINT, auth=BearerAuth(self.BEARER_TOKEN))
+        response.raise_for_status()
+        print(response.status_code)
+        return response.json()
+        # debug below
+        # return {'prices': [{'city': 'Paris', 'iataCode': 'd', 'lowestPrice': 54, 'id': 2},
+        # {'city': 'Berlin', 'iataCode': 'd', 'lowestPrice': 42, 'id': 3}, {'city': 'Tokyo', 'iataCode': 'd',
+        # 'lowestPrice': 485, 'id': 4}, {'city': 'Sydney', 'iataCode': 'd', 'lowestPrice': 551, 'id': 5},
+        # {'city': 'Istanbul', 'iataCode': 'd', 'lowestPrice': 95, 'id': 6}, {'city': 'Kuala Lumpur', 'iataCode': '',
+        # 'lowestPrice': 414, 'id': 7}, {'city': 'New York', 'iataCode': '', 'lowestPrice': 240, 'id': 8},
+        # {'city': 'San Francisco', 'iataCode': 'd', 'lowestPrice': 260, 'id': 9}, {'city': 'Cape Town', 'iataCode':
+        # '', 'lowestPrice': 378, 'id': 10}]}
+
+    def get_iata_codes(self):
+        # TODO search for the actual string
+        return "TESTING"
+
+    def update_sheet_row(self, row_id, change_json):
+        edit_endpoint = self.ENDPOINT + f"/{row_id}"
+        response = requests.put(url=edit_endpoint, auth=BearerAuth(self.BEARER_TOKEN), json=change_json)
+        response.raise_for_status()
+        print(response.status_code)
+
+
+class BearerAuth(requests.auth.AuthBase):
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers["authorization"] = "Bearer " + self.token
+        return r
--- Day39FlightFinder/main.py
+++ Day39FlightFinder/main.py
@@ -1,1 +1,24 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
+# program requirements.
+
+from data_manager import DataManager
+
+# load sheet data
+data_manger = DataManager()
+sheet_data = data_manger.get_sheet_content()["prices"]
+
+
+# prepare IATA codes & update sheet row
+rows_empty_iata = [city for city in sheet_data if len(city["iataCode"]) == 0]
+
+for city in rows_empty_iata:
+    row_id = city["id"]
+
+    change_json = {
+        "price": {
+            "iataCode": data_manger.get_iata_codes()
+        }
+    }
+
+    # send prepared data
+    data_manger.update_sheet_row(row_id, change_json)
