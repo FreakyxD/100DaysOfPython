d0278fa5fd2bdf180b0a9f32b5c5023c7df5ecdb
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-12-03 15:04:19 +0100

    refactor

--- Day0UsefulCode/TelegramBot/main.py
+++ Day0UsefulCode/TelegramBot/main.py
@@ -1,10 +1,12 @@
 import requests
 from auth import TELEGRAM_BOT_TOKEN, TELEGRAM_BOT_CHAT_ID
 
+
 class TelegramBot:
     def __init__(self):
         self.TOKEN = TELEGRAM_BOT_TOKEN
         self.CHAT_ID = TELEGRAM_BOT_CHAT_ID
+
     def send_message_to_telegram_bot(self, message):
         telegram_bot_url = "https://api.telegram.org/bot" + self.TOKEN + "/sendMessage"
         telegram_parameters = {
