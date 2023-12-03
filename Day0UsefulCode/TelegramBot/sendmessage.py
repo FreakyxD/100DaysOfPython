0c4d91145be341ce2f19ea6b0543d841220e7a35
freaky <24923493+FreakyxD@users.noreply.github.com>
2023-12-03 13:22:20 +0100

    converting to class for better interoperability

--- Day0UsefulCode/TelegramBot/sendmessage.py
+++ Day0UsefulCode/TelegramBot/sendmessage.py
@@ -1,14 +1,17 @@
 import requests
 from auth import TELEGRAM_BOT_TOKEN, TELEGRAM_BOT_CHAT_ID
 
-
-def send_message_to_telegram_bot(message):
-    telegram_bot_url = "https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendMessage"
-    telegram_parameters = {
-        "chat_id": TELEGRAM_BOT_CHAT_ID,
-        "parse_mode": "Markdown",
-        "text": message
-    }
-    telegram_response = requests.get(url=telegram_bot_url, params=telegram_parameters)
-    telegram_response.raise_for_status()
-    print(f"Telegram response: {telegram_response}")
+class TelegramBot:
+    def __init__(self):
+        self.TOKEN = TELEGRAM_BOT_TOKEN
+        self.CHAT_ID = TELEGRAM_BOT_CHAT_ID
+    def send_message_to_telegram_bot(self, message):
+        telegram_bot_url = "https://api.telegram.org/bot" + self.TOKEN + "/sendMessage"
+        telegram_parameters = {
+            "chat_id": self.CHAT_ID,
+            "parse_mode": "Markdown",
+            "text": message
+        }
+        telegram_response = requests.get(url=telegram_bot_url, params=telegram_parameters)
+        telegram_response.raise_for_status()
+        print(f"Telegram response: {telegram_response}")
