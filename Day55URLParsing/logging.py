ebf1d4a9e2e4cfb01f2d7bc1b2aa95664a3c3dc1
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-01-23 22:33:31 +0100

    optimized print statement by directly printing the tuple we got from *args

--- Day55URLParsing/logging.py
+++ Day55URLParsing/logging.py
@@ -3,14 +3,11 @@ inputs = [1, 2, 3]
 
 def logging_decorator(function):
     def wrapper(*args):
-        a = args[0]
-        b = args[1]
-        c = args[2]
-
-        print(f"You called {function.__name__}({a}, {b}, {c})")
-        result = function(a, b, c)
+        print(f"You called {function.__name__}{args}")
+        result = function(args[0], args[1], args[2])
         print(f"It returned: {result}")
         return result
+
     return wrapper
 
 
