74918ff60c3562e0cc5cfad391f9a251aadb61d8
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-01-23 22:23:36 +0100

    logging functionality using decorators

---
+++ Day55URLParsing/logging.py
@@ -0,0 +1,22 @@
+inputs = [1, 2, 3]
+
+
+def logging_decorator(function):
+    def wrapper(*args):
+        a = args[0]
+        b = args[1]
+        c = args[2]
+
+        print(f"You called {function.__name__}({a}, {b}, {c})")
+        result = function(a, b, c)
+        print(f"It returned: {result}")
+
+    return wrapper
+
+
+@logging_decorator
+def a_function(a, b, c):
+    return a * b * c
+
+
+a_function(inputs[0], inputs[1], inputs[2])
