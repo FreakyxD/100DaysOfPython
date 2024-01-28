1d34d2dce81989bd33584753c8e1d9619584a55b
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-01-28 12:52:17 +0100

    using css with flask

--- Day56HTMLFlask/server.py
+++ Day56HTMLFlask/server.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route("/cv")
+def cv():
+    return render_template("cv.html")
+
 @app.route("/")
 def landing_page():
-    return render_template("cv.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
