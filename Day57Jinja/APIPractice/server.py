32e7278ac425b70916abe94f07c9274e34100d39
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-02-01 11:58:00 +0100

    Integrated APIs

--- Day57Jinja/APIPractice/server.py
+++ Day57Jinja/APIPractice/server.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template, redirect, url_for
+import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-def get_age():
-    pass
+def get_age(input_name):
+    response = requests.get(f"https://api.agify.io?name={input_name}")
+    response.raise_for_status()
+    return response.json()["age"]
 
 
-def get_gender():
-    pass
-
-
-age = 22
-gender = "female"
+def get_gender(input_name):
+    response = requests.get(f"https://api.genderize.io?name={input_name}")
+    response.raise_for_status()
+    return response.json()["gender"]
 
 
 @app.route("/")
@@ -22,7 +23,9 @@ def landing_page():
 
 @app.route("/guess/<name>")
 def guess(name):
-    return render_template("guess.html", name=name.title(), age=age, gender=gender)
+    age = get_age(name)
+    gender = get_gender(name)
+    return render_template("guess.html", person_name=name.title(), person_age=age, person_gender=gender)
 
 
 if __name__ == "__main__":
