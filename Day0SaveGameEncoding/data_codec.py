e616100724073a7616aa7622fb10f97728adf0f3
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-02-04 16:54:53 +0100

    docstrings for most important functions

--- Day0SaveGameEncoding/data_codec.py
+++ Day0SaveGameEncoding/data_codec.py
@@ -15,6 +15,17 @@ def de_obfuscate_data(obfuscated_string):
 
 
 def encode_to_base64(data_dict, apply_obfuscation):
+    """
+    Encodes a dictionary to a Base64 string with optional obfuscation.
+
+    Parameters:
+        data_dict (dict): The dictionary to encode.
+        apply_obfuscation (bool): Whether to apply obfuscation to the data.
+
+    Returns:
+        str: The encoded (and possibly obfuscated) data in Base64 string format.
+    """
+
     # convert the dictionary to a JSON string
     json_string = json.dumps(data_dict, indent=4)
 
@@ -33,6 +44,19 @@ def encode_to_base64(data_dict, apply_obfuscation):
 
 
 def decode_from_base64(encoded_string, obfuscation_applied):
+    """
+    Decodes a Base64 string back to a Python dictionary, with an option to de-obfuscate the data if obfuscation was
+    applied during encoding.
+
+    Parameters:
+        encoded_string (str): The Base64 encoded string to decode, which may have been obfuscated.
+        obfuscation_applied (bool): Indicates whether the data was obfuscated during encoding.
+        This should match the `apply_obfuscation` parameter used in `encode_to_base64`.
+
+    Returns:
+        dict: The decoded Python dictionary, with obfuscation removed if it was applied.
+    """
+
     # convert encoded string back to bytes
     encoded_data = encoded_string.encode("utf-8")
 
