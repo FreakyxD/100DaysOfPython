376a5bed4e681be178f72272957e64fdf2254f13
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-02-10 19:46:39 +0100

    fixing js

--- Day59BlogPart2/main.py
+++ Day59BlogPart2/main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return render_template("index.html", css_url=url_for('static', filename='css/styles.css'))
+    return render_template("index.html", css_url=url_for("static", filename="css/styles.css"),
+                           js_url=(url_for("static", filename="js/scripts.js")))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
