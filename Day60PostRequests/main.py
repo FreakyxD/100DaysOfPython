d2701835e7e811ee310c2bb593841355ac174e38
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-02-17 13:58:22 +0100

    simple login prototype using POST

--- Day60PostRequests/main.py
+++ Day60PostRequests/main.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def receive_data():
+    error = None
+    if request.method == "POST":
+        print(request.form["username"])
+        print(request.form["password"])
+    return "Login ok"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
