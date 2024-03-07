947bb6572af83beedcaf4f79b99a0608b3160764
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-03-07 21:56:06 +0100

    switching from list creation to an sql database

--- Day63SQLite/main.py
+++ Day63SQLite/main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 '''
 Red underlines? Install the required packages first: 
@@ -15,24 +18,56 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 
-all_books = []
+
+# Create database #
+class Base(DeclarativeBase):
+    pass
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+# Initialise the app with the extension
+db.init_app(app)
+
+
+# Create table
+class Books(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    # This will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def home():
+    # Read all records
+    result = db.session.execute(db.select(Books).order_by(Books.title))
+    all_books = result.scalars()
     return render_template("index.html", all_books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        all_books.append(
-            {
-                "title": request.form.get("book_name"),
-                "author": request.form.get("book_author"),
-                "rating": request.form.get("rating"),
-            }
+        # Create record
+        new_book = Books(
+            title=request.form.get("book_name"),
+            author=request.form.get("book_author"),
+            rating=request.form.get("rating"),
         )
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for("home"))
     return render_template("add.html")
 
