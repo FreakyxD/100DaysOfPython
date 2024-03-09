006f7b86c9e748ef496fb78e82ce4078d086e0ca
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-03-09 14:43:49 +0100

    Implementing feature to change ratings

--- Day63SQLite/main.py
+++ Day63SQLite/main.py
@@ -72,5 +72,19 @@ def add():
     return render_template("add.html")
 
 
+@app.route("/edit", methods=["GET", "POST"])
+def edit_rating():
+    if request.method == "GET":
+        book_id = request.args.get("id")
+        book_selected = db.get_or_404(Books, book_id)
+        return render_template("edit-rating.html", book=book_selected)
+    elif request.method == "POST":
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Books, book_id)
+        book_to_update.rating = request.form["new_rating"]
+        db.session.commit()
+        return redirect(url_for("home"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
