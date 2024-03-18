8460660bea3993a5002d32fec13f2302d8dfd368
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-03-18 18:18:06 +0100

    simplify function

--- Day66RestAPICafe/main.py
+++ Day66RestAPICafe/main.py
@@ -55,11 +55,10 @@ def home():
 # HTTP GET - Read Record
 @app.route("/random") # methods=["GET"] can be omitted
 def get_random_cafe():
-    row_count = db.session.query(Cafe.id).count()
-    choice = random.randint(1, row_count)
-
-    cafe = db.session.execute(db.select(Cafe).where(Cafe.id == choice)).scalar()
-    return f"{cafe.name}"
+    row_count = db.session.query(Cafe).count()
+    choice_id = random.randint(1, row_count)
+    cafe = db.get_or_404(Cafe, choice_id)
+    return f"#{choice_id} - {cafe.name}"
 
 # HTTP POST - Create Record
 
