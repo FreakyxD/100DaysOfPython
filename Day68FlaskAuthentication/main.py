099d715727d28d741005786f4197e56c6ad4d35a
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-04-09 14:54:57 +0200

    this flash won't really be visible to the user

--- Day68FlaskAuthentication/main.py
+++ Day68FlaskAuthentication/main.py
@@ -65,7 +65,6 @@ def register():
         db.session.commit()
 
         login_user(new_user)
-        flash('Registration successful.')
         return render_template("secrets.html", username=new_user.name)
     return render_template("register.html")
 
