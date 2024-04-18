c2264bcf4ce0c64845fd37d8599962cb230f91e0
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-04-18 17:44:27 +0200

    optimize logic

--- Day69BlogFinal/main.py
+++ Day69BlogFinal/main.py
@@ -72,10 +72,7 @@ with app.app_context():
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if current_user.is_authenticated:
-            if current_user.id != 1:
-                return abort(403)
-        else:
+        if not current_user.is_authenticated or current_user.id != 1:
             return abort(403)
         return f(*args, **kwargs)
     return decorated_function
