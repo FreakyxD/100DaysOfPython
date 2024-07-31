4635c46278e8f7df5382084cb3cb388a30c977d4
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-07-31 22:10:50 +0200

    init game method, move outside game loop

--- Day85TicTacToe/game.py
+++ Day85TicTacToe/game.py
@@ -6,16 +6,19 @@ game_over = False
 my_board = Board()
 
 
+def init_game():
+    my_board.get_shapes()
+    my_board.set_shape()
+    my_board.get_shapes()
+
+
 def game():
     global game_over
     print(logo)
 
+    init_game()
     while not game_over:
-        my_board.display_board()
-
-        my_board.get_shapes()
-        my_board.set_shape()
-        my_board.get_shapes()
+        #my_board.display_board()
 
         game_over = True
 
