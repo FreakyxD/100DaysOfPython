57f8708550f33b300f46e01531731ddfe1860ec4
freaky <24923493+FreakyxD@users.noreply.github.com>
2024-09-24 01:16:42 +0200

    implementing speed increase and documentation

--- Day87Breakout/ball.py
+++ Day87Breakout/ball.py
@@ -8,26 +8,30 @@ class Ball(Turtle):
         self.color("white")
         self.penup()
 
+        # Set initial ball speed based on FPS
         if fps == 60:
-            ball_speed = 1.67 # movement per frame at 60 fps
+            ball_speed = 1.67
         else:
-            ball_speed = 3.33 # movement per frame at 30 fps
-
+            ball_speed = 3.33
 
         self.x_move = ball_speed
         self.y_move = ball_speed
+        self.speed_factor = 1.0  # Multiplier for speed, starting at 1.0
 
     def move(self):
-        new_x = self.xcor() + self.x_move
-        new_y = self.ycor() + self.y_move
+        """Move the ball by updating its position based on current speed."""
+        new_x = self.xcor() + self.x_move * self.speed_factor
+        new_y = self.ycor() + self.y_move * self.speed_factor
         self.goto(new_x, new_y)
 
     def bounce_x(self):
+        """Reverse the horizontal direction of the ball."""
         self.x_move *= -1
 
     def bounce_y(self):
+        """Reverse the vertical direction of the ball."""
         self.y_move *= -1
 
-    def increase_speed(self, boost):
-        self.x_move += abs(boost) if self.x_move > 0 else -abs(boost)
-        self.y_move += abs(boost) if self.y_move > 0 else -abs(boost)
+    def increase_speed(self):
+        """Increase ball speed by scaling up the speed factor."""
+        self.speed_factor *= 1.1  # Increase speed by 10%
